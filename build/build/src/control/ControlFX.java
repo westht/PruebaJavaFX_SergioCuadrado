a0f90ccf893f7c10b2f20591d4bae575522dcbdf
Sergio Cuadrado <sergiocv97@gmail.com>
2017-12-05 19:03:04 +0100

    Commit 6

---
+++ build/build/src/control/ControlFX.java
@@ -0,0 +1,47 @@
+package control;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+/**
+ * Este controlador es la Prueba de Desarrollo de Interfaces. Esta clase contiene las operaciones necesarias para
+ * la funcionalidad del programa.
+ * 
+ * @author Sergio Cuadrado
+ * 
+ * 
+ * @version 1.6
+ *
+ */
+
+public class ControlFX {
+	public void NuevaScreen() {
+        try {
+            // Load the fxml file and create a new stage for the popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainJavaFX.class.getResource("../vista/FreeSolo.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage sendStage = new Stage();
+            sendStage.setTitle("FREESOLOYOLO");
+            sendStage.initStyle(StageStyle.UNDECORATED);
+            Scene scene = new Scene(page);
+             sendStage.setScene(scene);
+
+            
+            sendStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	public void EndThePain(){
+		System.exit(0);
+	}
+
+}
---
+++ build/build/src/control/MainJavaFX.java
@@ -0,0 +1,55 @@
+package control;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+
+/**
+ * Este proyecto es la Prueba de Desarrollo de Interfaces. Se trata de una ventana
+ * login y de otra ventana llamada FreeSolo.
+ * 
+ * @author Sergio Cuadrado
+ * 
+ * 
+ * @version 1.6
+ *
+ */
+public class MainJavaFX extends Application {
+	private AnchorPane rootLayout;	
+	Stage stage;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			stage = primaryStage;
+			loadWindow();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	public void loadWindow(){
+		try {
+
+			FXMLLoader loader=new FXMLLoader(getClass().getResource("../vista/Login.fxml"));
+	        AnchorPane pane=loader.load();
+	        stage.initStyle(StageStyle.UNDECORATED);
+	        stage.setScene(new Scene(pane));
+	        stage.show();
+ 
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
